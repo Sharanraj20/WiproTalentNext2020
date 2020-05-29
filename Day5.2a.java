40d9aeb75234b20c514b5660da19f55905218ade
Sharan Raj G <60322880+Sharanraj20@users.noreply.github.com>
2020-05-29 18:44:03 +0530

    Create Day5.2a.java

---
+++ Day5.2a.java
@@ -0,0 +1,23 @@
+int leftSum = 0;
+        int totalSum = 0;
+        
+        int i = 0;
+        int size = arr.size();
+        while(i < size) {
+            totalSum += arr.get(i);
+            i++;
+        }
+        
+        i = 0;
+        while(i < size) {
+            if(totalSum - arr.get(i) == leftSum) {
+                return i;
+            } else {
+                leftSum += arr.get(i);
+                totalSum -= arr.get(i);
+            }
+            i++;
+        }
+        
+        return -1;
+    }
